8de5839dd4fd1da6157497e801c1e6553e400d1d
tuladhar07 <tuladhar135@gmail.com>
2023-01-18 18:14:14 +0545

    naya maal

--- backend/Users.js
+++ backend/Users.js
@@ -1,12 +1,34 @@
+const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
-
+var bcrypt= require ('bcryptjs');
+var Jwt= require ('jsonwebtoken');
 const userSchema = new mongoose.Schema({
+  
     username:String,
     email: String,
-    birthday: String,
+  
     ContactNo: String,
-    password: String,
-    confirmPassword: String
+    password: {
+        type:String,
+        select:false
+    },
+    confirmPassword:{
+        type: String,
+        select:false 
+        
+    },
+    verified: Boolean
+    
+     
 });
+
+
+userSchema.pre ('save', function(next){
+    var salt= bcrypt.genSaltSync(10);
+    this.password= bcrypt.hashSync(this.password, salt);
+    console.log(this.password)
+    next();
+})
+
 
 module.exports = mongoose.model('users', userSchema);
